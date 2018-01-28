eb0a444cb70790b2cb452eb804f99a0c3a368876
AHoobler <ashley.hoobler@gmail.com>
2018-01-28 10:20:16 -0500

    week two, loops and methods

--- PracticeProblems1/Program.cs
+++ PracticeProblems1/Program.cs
@@ -93,79 +93,79 @@ namespace PracticeProblems1
             // Week One, Day Three
 
 
-            
-            Console.WriteLine("Please enter your name");
-            string userName = Console.ReadLine();
 
-            Console.WriteLine("Name one of your friends");
-            string friendOne = Console.ReadLine();
+            //Console.WriteLine("Please enter your name");
+            //string userName = Console.ReadLine();
 
-            Console.WriteLine("Name a second friend");
-            string friendTwo = Console.ReadLine();
+            //Console.WriteLine("Name one of your friends");
+            //string friendOne = Console.ReadLine();
 
-            Console.WriteLine("Name a thrid friend");
-            string friendThree = Console.ReadLine();
+            //Console.WriteLine("Name a second friend");
+            //string friendTwo = Console.ReadLine();
 
-            Console.WriteLine("Who are you faster than?");
-            string runner = Console.ReadLine();
+            //Console.WriteLine("Name a thrid friend");
+            //string friendThree = Console.ReadLine();
 
-            // Lisa's cookies, one person
+            //Console.WriteLine("Who are you faster than?");
+            //string runner = Console.ReadLine();
 
-            double valueOne = 7;
-            double valueTwo = 2;
-            double valueThree = valueOne - valueTwo;
-            //Console.WriteLine(valueOne - valueTwo);
+            //// Lisa's cookies, one person
 
-            Console.WriteLine("{0} needs to add {1} cups of sugar.", userName, valueThree);
+            //double valueOne = 7;
+            //double valueTwo = 2;
+            //double valueThree = valueOne - valueTwo;
+            ////Console.WriteLine(valueOne - valueTwo);
 
-            //Mike and three friends, 4 people
-            valueOne = 13;
-            valueTwo = 4;
-            valueThree = valueOne * valueTwo;
-            //Console.WriteLine(valueOne * valueTwo);
+            //Console.WriteLine("{0} needs to add {1} cups of sugar.", userName, valueThree);
 
-            Console.WriteLine("The total cost for {0}, {1}, {2}, and {3} would be {4} dollars.", userName, friendOne, friendTwo, friendThree, valueThree);
+            ////Mike and three friends, 4 people
+            //valueOne = 13;
+            //valueTwo = 4;
+            //valueThree = valueOne * valueTwo;
+            ////Console.WriteLine(valueOne * valueTwo);
 
-            //Diapers, one person
-            valueOne = 40;
-            valueTwo = 8;
-            valueThree = valueOne / valueTwo;
-            // Console.WriteLine(valueOne / valueTwo);
+            //Console.WriteLine("The total cost for {0}, {1}, {2}, and {3} would be {4} dollars.", userName, friendOne, friendTwo, friendThree, valueThree);
 
-            Console.WriteLine("{0} can buy {1} boxes of diapers with $40.", userName, valueThree);
+            ////Diapers, one person
+            //valueOne = 40;
+            //valueTwo = 8;
+            //valueThree = valueOne / valueTwo;
+            //// Console.WriteLine(valueOne / valueTwo);
 
-            //Trevon's chores, one person
-            valueOne = 41;
-            valueTwo = 29;
-            valueThree = valueOne - valueTwo;
-            //Console.WriteLine(valueOne - valueTwo);
+            //Console.WriteLine("{0} can buy {1} boxes of diapers with $40.", userName, valueThree);
 
-            Console.WriteLine("{0} recieved {1} dollars for cleaning the attic.", userName, valueThree);
+            ////Trevon's chores, one person
+            //valueOne = 41;
+            //valueTwo = 29;
+            //valueThree = valueOne - valueTwo;
+            ////Console.WriteLine(valueOne - valueTwo);
 
-            //Julia runs, two people
-            valueOne = 47;
-            valueTwo = 30;
-            valueThree = valueOne - valueTwo;
-            //Console.WriteLine(valueOne - valueTwo);
+            //Console.WriteLine("{0} recieved {1} dollars for cleaning the attic.", userName, valueThree);
 
-            Console.WriteLine("{0} ran 47 miles, while {1} only ran {2} miles.", userName, runner, valueThree);
+            ////Julia runs, two people
+            //valueOne = 47;
+            //valueTwo = 30;
+            //valueThree = valueOne - valueTwo;
+            ////Console.WriteLine(valueOne - valueTwo);
 
-            // envelopes, one person
-            valueOne = 12;
-            valueTwo = 3;
-            valueThree = valueOne / valueTwo;
-            //Console.WriteLine(valueOne / valueTwo);
+            //Console.WriteLine("{0} ran 47 miles, while {1} only ran {2} miles.", userName, runner, valueThree);
 
-            Console.WriteLine("{0} is able to buy {1} box of envelopes with $12 dollars.", userName, valueThree);
+            //// envelopes, one person
+            //valueOne = 12;
+            //valueTwo = 3;
+            //valueThree = valueOne / valueTwo;
+            ////Console.WriteLine(valueOne / valueTwo);
 
-            //Norachi's salad, one person
-            valueOne = 27.10;
-            valueTwo = 5.12;
-            valueThree = valueOne + valueTwo;
-            // Console.WriteLine(valueOne + valueTwo);
+            //Console.WriteLine("{0} is able to buy {1} box of envelopes with $12 dollars.", userName, valueThree);
 
-            Console.WriteLine("Before buying salad, " + userName + " had " + valueThree + " dollars.");
-            
+            ////Norachi's salad, one person
+            //valueOne = 27.10;
+            //valueTwo = 5.12;
+            //valueThree = valueOne + valueTwo;
+            //// Console.WriteLine(valueOne + valueTwo);
+
+            //Console.WriteLine("Before buying salad, " + userName + " had " + valueThree + " dollars.");
+
 
             /*
             // MadLib Game
@@ -219,8 +219,145 @@ namespace PracticeProblems1
             Console.WriteLine("When {0} the river, swim {1}. You don't want to wake them up and make them {2}!", inputSeven, inputEight, inputNine);
             Console.WriteLine("Whatever you do, if you have an open wound, try to find another way to get back to the {0}.", inputTen);
             Console.WriteLine("Piranhas are attracted to fresh {0} and will most likely take a bite out of your {1} if you {2} in the water!", inputEleven, inputTwelve, inputThirteen);
-*/
+            */
+
+            // week two, day two loops
+
+            //populate the array with a for loop
+
+            //create a for loop
+            //print every other element to console (%)
+
+            //create an array of 25 elements
+
+            //int[] practiceArray = new int[25];
+            //practiceArray[0] = 1;
+            //practiceArray[1] = 2;
+            //practiceArray[2] = 3;
+            //practiceArray[3] = 4;
+            //practiceArray[4] = 5;
+            //practiceArray[5] = 6;
+            //practiceArray[6] = 7;
+            //practiceArray[7] = 8;
+            //practiceArray[8] = 9;
+            //practiceArray[9] = 10;
+            //practiceArray[10] = 11;
+            //practiceArray[11] = 12;
+            //practiceArray[12] = 13;
+            //practiceArray[13] = 14;
+            //practiceArray[14] = 15;
+            //practiceArray[15] = 16;
+            //practiceArray[16] = 17;
+            //practiceArray[17] = 18;
+            //practiceArray[18] = 19;
+            //practiceArray[19] = 20;
+            //practiceArray[20] = 21;
+            //practiceArray[21] = 22;
+            //practiceArray[22] = 23;
+            //practiceArray[23] = 24;
+            //practiceArray[24] = 25;
+
+
+
+            //for (int i = 0; i <= practiceArray.Length;i++);
+            //{ Console.WriteLine("Enter a value for" + practiceArray[i]);
+            //    practiceArray[i] = int.Parse(Console.ReadLine());
+            //    //Console.ReadLine(practiceArray[i]);
+            //}
+
+
+            // for (steup; test expression; increment) {body}
+
+
+            //int[] practiceNumbers = new int[25];
+            //for (int i = 0; i < practiceNumbers.Length; i++)
+            //{
+            //    Console.WriteLine("Enter Value for" + i); // don't need to put variable name on the i index  
+            //    practiceNumbers[i] = int.Parse(Console.ReadLine());   
+            //}
+
+
+            //random number generator, ANY RANGE OF NUMBERS, can be zero, will not be 100
+            Random r = new Random();
+
+            int[] rArray = new int[25];
+
+            for (int i = 0; i < rArray.Length; i++)
+            {
+                rArray[i] = r.Next(0, 100);
+                Console.WriteLine(rArray[i]);
+            }
+
+
+            // week 2, day 4
+            /*
+            The homework problems are as follows: 
+            1) Create a method which uses two user inputs and returns the greatest(use a loop to receive user inputs), 
+            2) Create a method which uses two user inputs and returns if they are equal(use a loop to receiver user inputs), 
+            3) Create a method which takes in the user's three favorite foods and returns and prints the three foods in a sentence (use a loop to receiver user inputs), and 
+            4) Create a method which takes two sides of a triangle and returns the third side (use a loop to receiver user inputs).
+            */
+
+            //1
+
+            int[] qTwoArray = new int[2];
+            
+            for (int i = 0; i < qTwoArray.Length; i++)
+            {
+                Console.WriteLine("Please enter a number");
+                qTwoArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine((qTwoArray));
+
+            //2
+            int[] qOneArray = new int[2];
+            for (int i = 0; i < qTwoArray.Length; i++)
+            {
+                Console.WriteLine("Please enter a number");
+                qOneArray[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            // tutoring METHODS
+            //*string messageOne = "Have a nice day";
+            //*string messageTwo = "Welcome friends";
+
+            // void method does not need paramenters Console.WriteLine(Message(messageOne, messageTwo)); // call the method back to the main method 
+            //*Message();
+            //Console.WriteLine("Enter your name");
+            //string fName = Console.ReadLine();
+
+            //**Console.WriteLine("Enter your number");
+            //**int fNum = int.Parse(Console.ReadLine());
+            //**Console.WriteLine("Enter your number");
+            //**int sNum = int.Parse(Console.ReadLine());
+
+            //**Console.WriteLine (Sum(fNum, sNum)); // int needs a return
+        }
+
+      // static void Message(string name) // (string phraseOne, string phraseTwo) // doorway that sends information to the body of the message, void method is a one way door. 
+           //** static int Sum(int numOne, int numTwo)
+        //{
+           //** int sum;
+           //** sum = numOne + numTwo;
+            //Console.WriteLine("Welcome friend " + name);
+            //Console.WriteLine("Have a nice day");
+
+           //** return sum;
+            //     Console.WriteLine("Welcome friends");
+            //     Console.WriteLine("Have a nice day"); // no return, not getting called back
+
+
+
+            //     //return phraseTwo + " \n" + phraseOne; 
+
+        //1
+
+        static int [] Greatest(int[]  )
+        {
 
         }
-    }
+            
+        }
 }
